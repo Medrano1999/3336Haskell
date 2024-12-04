9b502965ccf06c6c69fd5ccc25a26f59f271dae6
Medrano1999 <147267160+Medrano1999@users.noreply.github.com>
2024-12-04 22:05:53 +0000

    final project

--- Final Project/Evaluator.hs
+++ Final Project/Evaluator.hs
@@ -1,91 +1,114 @@
-module Evaluator (evaluateP) where
+module Evaluator where
 
 import Syntax
-import System.IO.Unsafe
+import System.IO.Unsafe  -- Needed for printing side effects within evaluation
 
--- Function to evaluate a program
+-- Evaluates an entire program, starting from an empty environment
 evaluateP :: Program -> Result
-evaluateP (BeginEnd ss) = evaluateSS ss [] []
+evaluateP (BeginEnd ss) = evaluateSS ss []
 
--- Function to evaluate a sequence of statements
-evaluateSS :: Statements -> Env -> FuncEnv -> Result
-evaluateSS (End s) env fenv = evaluateS s env fenv
-evaluateSS (Seq s ss) env fenv = case evaluateS s env fenv of
-    Valid env' -> evaluateSS ss env' fenv
-    Error msg -> Error msg
+-- Evaluates a sequence of statements
+evaluateSS :: Statements -> Env -> Result
+evaluateSS (End s) env = evaluateS s env  -- If there's only one statement, evaluate it
+evaluateSS (Seq s ss) env = case evaluateS s env of
+    Valid env' -> evaluateSS ss env'  -- If the statement is valid, continue with the rest
+    Error s -> Error s  -- If there's an error, return it
 
--- Function to evaluate a statement
-evaluateS :: Stmt -> Env -> FuncEnv -> Result
-evaluateS (Assign x t e) env fenv = case (evaluate e env fenv, t) of
-    (ValI vi, TypeI) -> Valid ((x, ValI vi) : env)
-    (ValB vb, TypeB) -> Valid ((x, ValB vb) : env)
-    (ValS vs, TypeS) -> Valid ((x, ValS vs) : env)
-    (ValA va, TypeA) -> Valid ((x, ValA va) : env)
-    (_, _) -> Error $ "Type mismatch for variable " ++ x
+-- Evaluates a single statement
+evaluateS :: Stmt -> Env -> Result
+-- Assignment: Evaluate the expression and assign it to the variable in the environment
+evaluateS (Assign x t e) env = case (evaluate e env, t) of
+    (ValI vi, TypeI) -> Valid ((x, ValI vi) : env)  -- Assign integer
+    (ValF vf, TypeF) -> Valid ((x, ValF vf) : env)  -- Assign float
+    (ValB vb, TypeB) -> Valid ((x, ValB vb) : env)  -- Assign boolean
+    (ValC vc, TypeC) -> Valid ((x, ValC vc) : env)  -- Assign character
+    (ValS vs, TypeS) -> Valid ((x, ValS vs) : env)  -- Assign string
+    (ValArray va, TypeArray) -> Valid ((x, ValArray va) : env)  -- Assign array
+    (_, _) -> Error $ "Type mismatch for " ++ x  -- Type mismatch error
 
-evaluateS (While e ss) env fenv = case evaluate e env fenv of
-    ValB False -> Valid env
-    ValB True -> case evaluateSS ss env fenv of
-                    Error msg -> Error msg
-                    Valid env' -> evaluateS (While e ss) env' fenv
-    _ -> Error "While condition must be boolean"
+-- While loop: Continuously evaluate the body while the condition is true
+evaluateS (While e ss) env = case evaluate e env of
+    ValB False -> Valid env  -- If condition is false, end loop
+    ValB True -> case evaluateSS ss env of
+        Valid env' -> evaluateS (While e ss) env'  -- If body is valid, loop again
+        Error em -> Error em  -- Error in body execution
+    _ -> Error "Condition in while loop must be boolean"  -- Invalid condition type
 
-evaluateS (DoWhile ss e) env fenv = case evaluateSS ss env fenv of
-    Error msg -> Error msg
-    Valid env' -> case evaluate e env' fenv of
-        ValB True -> evaluateS (DoWhile ss e) env' fenv
-        ValB False -> Valid env'
-        _ -> Error "Do-while condition must be boolean"
+-- Function Declaration
+evaluateS (FuncDecl name params body) env = Valid ((name, ValE "Function declared") : env)
 
-evaluateS (For init e step body) env fenv = case evaluateS init env fenv of
-    Error msg -> Error msg
-    Valid env' -> evaluateS (While e (sequenceSS body step)) env' fenv
+-- Return Statement
+evaluateS (Return e) env = case evaluate e env of
+    ValE err -> Error err  -- Return an error if evaluation fails
+    val -> Valid (("return", val) : env)
 
-evaluateS (Print e) env fenv = case evaluate e env fenv of
-    ValE msg -> unsafePerformIO (print msg >> return (Error msg))
-    v -> unsafePerformIO (print v >> return (Valid env))
+-- Do-While loop: Execute body at least once, then check condition to continue
+evaluateS (DoWhile body cond) env = case evaluateSS body env of
+    Error errMsg -> Error errMsg  -- Error in the body
+    Valid env' -> case evaluate cond env' of
+        ValB True -> evaluateS (DoWhile body cond) env'  -- Loop again if condition is true
+        ValB False -> Valid env'  -- Terminate if condition is false
+        _ -> Error "Condition in do-while loop must be boolean"  -- Invalid condition type
 
-evaluateS (Input x t) env fenv = unsafePerformIO $ do
-    putStrLn $ "Enter value for " ++ x ++ ":"
-    input <- getLine
-    let result = case t of
-            TypeI -> case reads input of
-                        [(val, "")] -> Valid ((x, ValI val) : env)
-                        _ -> Error $ "Invalid input for integer variable " ++ x
-            TypeB -> case input of
-                        "true" -> Valid ((x, ValB True) : env)
-                        "false" -> Valid ((x, ValB False) : env)
-                        _ -> Error $ "Invalid input for boolean variable " ++ x
-            TypeS -> Valid ((x, ValS input) : env)
-            _ -> Error $ "Unsupported input type for variable " ++ x
-    return result
+-- Print statement: Prints the value of an expression
+evaluateS (Print e) env = case evaluate e env of
+    ValE em -> unsafePerformIO (print em >> return (Error em))  -- Print an error message
+    v -> unsafePerformIO (print v >> return (Valid env))  -- Print value and return current environment
 
--- Function to evaluate an expression
-evaluate :: Expr -> Env -> FuncEnv -> Val
-evaluate (Value v) _ _ = v
-evaluate (IfElse c e1 e2) env fenv = case evaluate c env fenv of
-    ValB True -> evaluate e1 env fenv
-    ValB False -> evaluate e2 env fenv
-    _ -> ValE "Condition in if-else must be boolean"
+-- Evaluates an expression and returns a value
+evaluate :: Expr -> Env -> Val
+evaluate (Value v) _ = v  -- If it's a value, just return it
+evaluate (IfElse cond e1 e2) env = case evaluate cond env of
+    ValB True -> evaluate e1 env  -- If condition is true, evaluate the first expression
+    ValB False -> evaluate e2 env  -- If false, evaluate the second expression
+    _ -> ValE "Condition should be boolean"  -- Error if condition is not boolean
 
-evaluate (BinExpr e1 op e2) env fenv = case (evaluate e1 env fenv, evaluate e2 env fenv) of
-    (ValI i1, ValI i2) -> evaluateOp (ValI i1) op (ValI i2)
-    (ValB b1, ValB b2) -> evaluateOp (ValB b1) op (ValB b2)
-    (ValS s1, ValS s2) -> evaluateOp (ValS s1) op (ValS s2)
-    _ -> ValE "Operands must be of compatible types"
+-- Binary expression evaluation
+evaluate (BinExpr e1 op e2) env = case (evaluate e1 env, evaluate e2 env) of
+    (ValI i1, ValI i2) -> evaluateIntOp op i1 i2  -- Integer operations
+    (ValF f1, ValF f2) -> evaluateFloatOp op f1 f2  -- Float operations
+    (ValS s1, ValS s2) -> evaluateStringOp op s1 s2  -- String operations
+    (ValB b1, ValB b2) -> evaluateBoolOp op b1 b2  -- Boolean operations
+    _ -> ValE "Operands are of incompatible types"  -- Error if types do not match
 
-evaluate (Ref x) env _ = case lookup x env of
-    Nothing -> ValE "Variable not in scope"
-    Just v -> v
+-- Variable reference
+evaluate (Ref x) env = case lookup x env of
+    Nothing -> ValE ("Variable " ++ x ++ " not in scope")  -- Error if variable not found
+    Just v -> v  -- Return the value of the variable
 
--- Function to evaluate binary operations
-evaluateOp :: Val -> Op -> Val -> Val
-evaluateOp (ValI i1) Add (ValI i2) = ValI (i1 + i2)
-evaluateOp (ValI i1) Sub (ValI i2) = ValI (i1 - i2)
-evaluateOp (ValI i1) Mul (ValI i2) = ValI (i1 * i2)
-evaluateOp (ValI i1) Div (ValI i2) = if i2 == 0 then ValE "Division by zero" else ValI (i1 `div` i2)
-evaluateOp (ValI i1) GEq (ValI i2) = ValB (i1 >= i2)
-evaluateOp (ValB b1) And (ValB b2) = ValB (b1 && b2)
-evaluateOp (ValB b1) Or (ValB b2) = ValB (b1 || b2)
-evaluateOp (ValS s1) Concat (ValS s2) = ValS (s1 ++ s2)
-evaluateOp _ _ _ = ValE "Invalid operation"
+-- Function application: Evaluate the body of the function with the arguments provided
+evaluate (App (Func x _ body) args) env =
+    let newEnv = foldl (\acc (var, arg) -> case evaluate arg env of
+                    ValE err -> error err
+                    val -> (var, val) : acc) env (zip [x] args)  -- Update environment with arguments
+    in evaluate body newEnv  -- Evaluate the body of the function in the new environment
+
+-- If an expression doesn't match any of the above, return undefined
+evaluate _ _ = ValE "Undefined expression"
+
+-- Helper functions to evaluate different types of operations
+evaluateIntOp :: Op -> Int -> Int -> Val
+evaluateIntOp Add i1 i2 = ValI (i1 + i2)
+evaluateIntOp Sub i1 i2 = ValI (i1 - i2)
+evaluateIntOp Mul i1 i2 = ValI (i1 * i2)
+evaluateIntOp Div i1 i2
+    | i2 == 0 = ValE "Division by zero"
+    | otherwise = ValI (i1 `div` i2)
+evaluateIntOp Eq i1 i2 = ValB (i1 == i2)
+evaluateIntOp NotEq i1 i2 = ValB (i1 /= i2)
+evaluateIntOp Syntax.LT i1 i2 = ValB (i1 < i2)  -- Explicitly refer to Syntax.LT
+
+evaluateFloatOp :: Op -> Float -> Float -> Val
+evaluateFloatOp Add f1 f2 = ValF (f1 + f2)
+evaluateFloatOp Sub f1 f2 = ValF (f1 - f2)
+evaluateFloatOp Mul f1 f2 = ValF (f1 * f2)
+evaluateFloatOp Div f1 f2
+    | f2 == 0.0 = ValE "Division by zero"
+    | otherwise = ValF (f1 / f2)
+
+evaluateStringOp :: Op -> String -> String -> Val
+evaluateStringOp Concat s1 s2 = ValS (s1 ++ s2)
+
+evaluateBoolOp :: Op -> Bool -> Bool -> Val
+evaluateBoolOp Eq b1 b2 = ValB (b1 == b2)
+evaluateBoolOp NotEq b1 b2 = ValB (b1 /= b2)
--- Final Project/Main.hs
+++ Final Project/Main.hs
@@ -3,21 +3,51 @@ module Main where
 import Syntax
 import Evaluator
 
--- Example program to test
-sW :: Stmt -- while A >= 3 { print A; A = A - 1 }
-sW = While
-    (BinExpr (Ref "A") GEq (Value (ValI 3)))
-    (Seq
-        (Print (Ref "A"))
-        (End (Assign "A" TypeI (BinExpr (Ref "A") Sub (Value (ValI 1)))))
-    )
-
-progW :: Program
-progW = BeginEnd (Seq (Assign "A" TypeI (Value (ValI 5))) (End sW))
-
--- Main function for running the program
+-- Main function to run all examples
 main :: IO ()
 main = do
-    putStrLn "Running example programs:"
-    let result1 = evaluateP progW
-    putStrLn $ "Result of progW: " ++ show result1
+    putStrLn "Running example programs:\n"
+
+    -- Float Example
+    let floatExample = Assign "X" TypeF (Value (ValF 3.14))
+    print (evaluateS floatExample [])
+
+    -- Character Example
+    let charExample = Assign "Y" TypeC (Value (ValC 'a'))
+    print (evaluateS charExample [])
+
+    -- String Manipulation Example
+    let stringManipulationExample = Assign "Z" TypeS (BinExpr (Value (ValS "Hello ")) Concat (Value (ValS "World")))
+    print (evaluateS stringManipulationExample [])
+
+    -- Equality Comparison Example
+    let equalityComparisonExample = Assign "A" TypeB (BinExpr (Value (ValI 5)) Eq (Value (ValI 5)))
+    print (evaluateS equalityComparisonExample [])
+
+    -- Inequality Comparison Example
+    let inequalityComparisonExample = Assign "B" TypeB (BinExpr (Value (ValI 5)) Syntax.LT (Value (ValI 10)))
+    print (evaluateS inequalityComparisonExample [])
+
+    -- Array/List Example
+    let arrayExample = Assign "L" TypeArray (Value (ValArray [ValI 1, ValI 2, ValI 3]))
+    print (evaluateS arrayExample [])
+
+    -- Function Definition Example
+    let functionDefinitionExample = FuncDecl "sum" ["a", "b"] (End (Return (BinExpr (Ref "a") Add (Ref "b"))))
+    print (evaluateS functionDefinitionExample [])
+
+    -- While Loop Example
+    let whileLoopExample = While
+            (BinExpr (Ref "Counter") Syntax.LT (Value (ValI 3)))  -- Condition is Counter < 3
+            (Seq
+                (Print (Ref "Counter"))  -- Print Counter
+                (End (Assign "Counter" TypeI (BinExpr (Ref "Counter") Add (Value (ValI 1))))))  -- Increment Counter
+    print (evaluateS whileLoopExample [("Counter", ValI 0)])
+
+    -- DoWhile Loop Example
+    let doWhileExample = DoWhile
+            (Seq
+                (Print (Ref "Counter"))  -- Print Counter
+                (End (Assign "Counter" TypeI (BinExpr (Ref "Counter") Add (Value (ValI 1))))))  -- Increment Counter
+            (BinExpr (Ref "Counter") Syntax.LT (Value (ValI 3)))  -- Condition is Counter < 3
+    print (evaluateS doWhileExample [("Counter", ValI 0)])
